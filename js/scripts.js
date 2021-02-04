66fd76f6c87c46c45ff92522fc7f39bb2d17b2f8
Woah169 <78505055+Woah169@users.noreply.github.com>
2021-02-04 16:08:23 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,15 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth/1.25, window.innerHeight/1.25 );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
 camera.position.z = 5;
 
-var speed = 1;
 
+//Camera Controls
+var speed = 1;
 var controls = new THREE.PointerLockControls(camera, renderer.domElement); 
-var clock = new THREE.Clock();
 renderer.domElement.addEventListener('click', function(){
 	controls.lock();
 },false);
-
 var keyboard = [];
 addEventListener('keydown', (e) =>{
 	keyboard[e.key] = true;
@@ -28,7 +22,6 @@ addEventListener('keyup', (e) =>{
 	keyboard[e.key] = false;
 });
 addEventListener('wheel', (e) => {
-	console.log("a")
 	if(e.deltaY  > 0){
 		speed *= 0.90;
 	}
@@ -36,7 +29,6 @@ addEventListener('wheel', (e) => {
 		speed *= 10/9;
 	}
   });
-
 function processKeys(){
 	if (keyboard['w']){
 		controls.moveForward(speed)
@@ -56,22 +48,8 @@ function processKeys(){
 }
 
 
-var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
-keyLight.position.set(-100, 0, 100).normalize();
-
-var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
-fillLight.position.set(100, 1000, 100).normalize();
-
-var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
-backLight.position.set(100, 0, -100).normalize();
-
-
-
-scene.add(keyLight);
-scene.add(fillLight);
-scene.add(backLight);
-
-function loadScene(){
+//Load Collision
+function loadScene(LevelID){
 
 	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 	var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -80,24 +58,25 @@ function loadScene(){
 
 	var objLoader = new THREE.OBJLoader();
 	
-	objLoader.load('Resources/collision.obj', function(object){
+	objLoader.load('stage/' + "aqa/a" + "/collision.obj", function(object){
 		object.scale.set(0.001,0.001,0.001)
 		object.position.set(0,0,0);
 		scene.add(object);
 	});
 }
 
+
+//Clear Collision
 function clearScene(){
 	scene.children = [];
+
+	//Add Lighting back
 	var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
 	keyLight.position.set(-100, 0, 100).normalize();
-
 	var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
 	fillLight.position.set(100, 1000, 100).normalize();
-
 	var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
 	backLight.position.set(100, 0, -100).normalize();
-
 	scene.add(keyLight);
 	scene.add(fillLight);
 	scene.add(backLight);
@@ -110,5 +89,4 @@ var animate = function () {
 
 	renderer.render(scene, camera);
 };
-
 animate();
