0fda8c0903641016d58241951a0ce070d806808c
Woah169 <78505055+Woah169@users.noreply.github.com>
2021-02-06 22:10:03 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -155,12 +155,15 @@ function loadPlacement(PlacementID){
 							Node.children[2].children[0].children[0].children[2].innerHTML * 0.001
 						);
 
-						cube.rotation.setFromQuaternion(new THREE.Quaternion(
+						var Rotation = new THREE.Quaternion(
 							Node.children[2].children[0].children[1].children[0].innerHTML,
 							Node.children[2].children[0].children[1].children[1].innerHTML,
 							Node.children[2].children[0].children[1].children[2].innerHTML,
 							Node.children[2].children[0].children[1].children[3].innerHTML,
-						), "XYZ");
+						);
+
+						cube.quaternion = Rotation;
+
 						
 						scene.add( cube );
 
