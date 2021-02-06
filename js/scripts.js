26ff234d9d2f8acd8b34c43cf724af10b1b10e99
Woah169 <78505055+Woah169@users.noreply.github.com>
2021-02-06 20:50:16 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 var col;
+var xmlDOM;
 var collisionGroups;
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth/1.25, window.innerHeight/1.25 );
@@ -134,8 +135,8 @@ function loadPlacement(PlacementID){
 	fetch("placement/"+PlacementID).then((response)=>{
 		response.text().then((xml)=>{
 			let parser = new DOMParser();
-			let xmlDOM = parser.parseFromString(xml, 'application/xml');
-			console.log(xmlDOM);
+			xmlDOM = parser.parseFromString(xml, 'application/xml').children[0];
+			
 		})
 	})
 
